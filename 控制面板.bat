04ff178a1659b1abb5d400e805aa7505cf3aba5c
balabala <local@research>
2026-08-25 09:30:50 +0800

    面板改控制台python启动+新建精读监听.bat(看门狗)；记录误杀watcher教训(踩坑#42)

--- 控制面板.bat
+++ 控制面板.bat
@@ -1,7 +1,8 @@
 @echo off
+echo %date% %time% bat-ran >> "%~dp0_launch_probe.log"
 chcp 65001 >nul
 cd /d "%~dp0"
-start "" pythonw 平台管理\panel_launch.py
+start "" /min python 平台管理\panel_launch.py
 timeout /t 2 /nobreak >nul
 start "" http://127.0.0.1:8777/
 exit
